31ba6ffec15d5311b790754e466b459b29af6b45
moitrau <62080348+moitrau@users.noreply.github.com>
2024-10-01 18:55:28 -0400

    Added gobuilda odometry tunning for road runner and meep meep

---
+++ MainFtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoAutonColorSensorAndroid.java
@@ -0,0 +1,49 @@
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "DemoAutonColorSensorAndroid")
+public class DemoAutonColorSensorAndroid extends LinearOpMode {
+
+    private ColorSensor colorSensor_REV_ColorRangeSensor;
+
+    /**
+     * This OpMode demonstrates the color and distance features of the REV sensor.
+     */
+    @Override
+    public void runOpMode() {
+        double distance;
+        double hue;
+
+        colorSensor_REV_ColorRangeSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                // Display distance info.
+                ((NormalizedColorSensor) colorSensor_REV_ColorRangeSensor).setGain(2);
+                distance = ((DistanceSensor) colorSensor_REV_ColorRangeSensor).getDistance(DistanceUnit.CM);
+                telemetry.addData("Dist to tgt (cm)", ((DistanceSensor) colorSensor_REV_ColorRangeSensor).getDistance(DistanceUnit.CM));
+                hue = Double.parseDouble(JavaUtil.formatNumber(JavaUtil.colorToHue(((NormalizedColorSensor) colorSensor_REV_ColorRangeSensor).getNormalizedColors().toColor()), 0));
+                telemetry.addData("Hue", Double.parseDouble(JavaUtil.formatNumber(hue, 0)));
+                // Use hue to determine if it's red, yellow, blue sample
+                if (distance <= 4 && hue >= 200 && hue <= 240) {
+                    telemetry.addData("Detection Status", "Blue Sample");
+                } else if (distance <= 4 && hue >= 80 && hue <= 100) {
+                    telemetry.addData("Detection Status", "Yellow Sample");
+                } else if (distance <= 4 && hue >= 0 && hue <= 60) {
+                    telemetry.addData("Detection Status", "Red Sample");
+                } else {
+                    telemetry.addData("Detection Status", "No Sample Detected !  Sample distance should be less than 4 cms from the sensor.");
+                }
+                telemetry.update();
+            }
+        }
+    }
+}
--- MainFtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicAuton.java
+++ MainFtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicAuton.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -20,19 +21,19 @@ public class BasicAuton extends LinearOpMode {
         waitForStart();
 
         if (isStopRequested()) return;
-
-        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                .splineTo(new Vector2d(30, 30), 0)
+        Pose2d startPose;
+        startPose = new Pose2d(-12, 65, Math.toRadians(-90));
+        drive.setPoseEstimate(startPose);
+        TrajectorySequence traj = drive.trajectorySequenceBuilder(startPose)
+                .forward(15)
+                .turn(Math.toRadians(90))
+                .forward(55)
+                .turn(Math.toRadians(45))
                 .build();
 
-        drive.followTrajectory(traj);
+        drive.followTrajectorySequence(traj);
 
-        sleep(2000);
 
-        drive.followTrajectory(
-                drive.trajectoryBuilder(traj.end(), true)
-                        .splineTo(new Vector2d(0, 0), Math.toRadians(180))
-                        .build()
-        );
+
     }
 }
