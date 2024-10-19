d7c0105742e63d96d5b14a75592b809c2dfdb378
moitrau <62080348+moitrau@users.noreply.github.com>
2024-10-18 23:36:12 -0400

    BlueAllianceYellowSample Updates

--- MainFtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MainFtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,17 +17,22 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-12, 65, Math.toRadians(-90)))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(9, 64, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(9,36,Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(37,37,Math.toRadians(-45)))
+                        .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                        .lineToLinearHeading(new Pose2d(58,40,Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                        .lineToLinearHeading(new Pose2d(58,40,Math.toRadians(-45)))
+                        .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                        //.lineToLinearHeading(new Pose2d(27,12,Math.toRadians(0)))
+                        .splineToLinearHeading(new Pose2d(32,12,Math.toRadians(-0)),Math.toRadians(-160))
                         //.forward(16)
                         //.turn(Math.toRadians(90))
                         //.forward(55)
                         //.turn(Math.toRadians(45))
                         //.forward(10)
-<<<<<<< Updated upstream
-                        .splineToLinearHeading(new Pose2d(50,55,Math.toRadians(45)),Math.toRadians(30))
-=======
-                        .splineToLinearHeading(new Pose2d(50,55,Math.toRadians(45)),Math.toRadians(210))
->>>>>>> Stashed changes
+                        //.splineToLinearHeading(new Pose2d(50,55,Math.toRadians(45)),Math.toRadians(30))
                         .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
