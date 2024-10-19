184623997f56890b5c5b8c5ccb7c31f07b87ce5a
moitrau <62080348+moitrau@users.noreply.github.com>
2024-10-19 00:20:19 -0400

    Updates for BAYS

--- MainFtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicAutonBAYS.java
+++ MainFtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicAutonBAYS.java
@@ -1,8 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
+
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,8 +11,8 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 /*
  * This is an example of a more complex path to really test the tuning.
  */
-@Autonomous(name = "BasicAuton")
-public class BasicAuton extends LinearOpMode {
+@Autonomous(name = "01_BasicAutonBlueAllianceYellowSample")
+public class BasicAutonBAYS extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -22,7 +21,7 @@ public class BasicAuton extends LinearOpMode {
 
         if (isStopRequested()) return;
         Pose2d startPose;
-        startPose = new Pose2d(-12, 65, Math.toRadians(-90));
+        startPose = new Pose2d(9, 64, Math.toRadians(90));
         drive.setPoseEstimate(startPose);
         TrajectorySequence traj = drive.trajectorySequenceBuilder(startPose)
                 //.forward(16)
@@ -30,7 +29,17 @@ public class BasicAuton extends LinearOpMode {
                 //.forward(55)
                 //.turn(Math.toRadians(45))
                 //.forward(10)
-                .splineToLinearHeading(new Pose2d(50,55,Math.toRadians(45)),Math.toRadians(90))
+                //.splineToLinearHeading(new Pose2d(50,55,Math.toRadians(45)),Math.toRadians(90))
+                .lineToLinearHeading(new Pose2d(9,36,Math.toRadians(90)))
+                .lineToLinearHeading(new Pose2d(37,37,Math.toRadians(-45)))
+                .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                .lineToLinearHeading(new Pose2d(58,40,Math.toRadians(-90)))
+                .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                .lineToLinearHeading(new Pose2d(58,40,Math.toRadians(-45)))
+
+                .lineToLinearHeading(new Pose2d(54,54,Math.toRadians(-135)))
+                .lineToLinearHeading(new Pose2d(27,12,Math.toRadians(0)))
+                .splineToLinearHeading(new Pose2d(32,12,Math.toRadians(-0)),Math.toRadians(-160))
                 .build();
 
         drive.followTrajectorySequence(traj);
